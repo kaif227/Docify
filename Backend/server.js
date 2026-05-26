93620623d74df5a54a976145e0f613109fe45510
kaif <kaifbhati58@gmail.com>
2026-05-26 12:29:23 +0530

    changs in apis

--- Backend/server.js
+++ Backend/server.js
@@ -1,36 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 
-const uploadRoutes =
-require("./routes/uploadRoutes");
+const uploadRoutes = require("./routes/uploadRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-
-res.json({
-
-message:
-"Backend running successfully"
-
+app.get("/", (req, res) => {
+  res.json({
+    message: "Backend running successfully",
+  });
 });
 
-});
+app.use("/api", uploadRoutes);
+const cors = require("cors");
 
-app.use(
-"/api",
-uploadRoutes
-);
+app.use(cors({
+origin:"*"
+}));
 
-const PORT=5000;
+const PORT = 5000;
 
-app.listen(PORT,()=>{
-
-console.log(
-`Server running on ${PORT}`
-);
-
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
